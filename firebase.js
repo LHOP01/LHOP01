a30d579c2fb805832efdc09de23b6e77dd74c568
LITE HACK OP <mayankxmarty1@gmail.com>
2026-07-13 21:37:42 +0530

    firebase.js
    Initialize Firebase app and database with configuration. Add saveKey function to store keys in the database.

--- firebase.js
+++ firebase.js
@@ -1,1 +1,33 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  set
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAQ5CO4sLv_8j22N0YJyQmz8hIZpMs6DhE",
+  authDomain: "lhop01-admin.firebaseapp.com",
+  databaseURL: "https://lhop01-admin-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "lhop01-admin",
+  storageBucket: "lhop01-admin.firebasestorage.app",
+  messagingSenderId: "382345315199",
+  appId: "1:382345315199:web:fafa4d6ee8d82e1d97104a"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+window.saveKey = function(key, days){
+
+    const id = push(ref(db,"keys"));
+
+    set(id,{
+        key:key,
+        validity:days,
+        status:"active",
+        created:Date.now()
+    });
+
+}
