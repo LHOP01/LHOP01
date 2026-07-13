b5481f8eef0fb9d1083c2fc06d1f2543d54dd8fc
LITE HACK OP <mayankxmarty1@gmail.com>
2026-07-13 21:19:29 +0530

    app.js
    Added functions to generate random keys and custom keys.

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+function randomPart(length){
+const chars="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+let out="";
+for(let i=0;i<length;i++){
+out+=chars[Math.floor(Math.random()*chars.length)];
+}
+return out;
+}
 
+function generateKey(){
+
+const key="LHOP01-"+randomPart(4)+"-"+randomPart(4);
+
+document.getElementById("result").innerHTML=key;
+
+}
+
+function generateCustomKey(){
+
+const key=document.getElementById("customKey").value;
+
+document.getElementById("result").innerHTML=key;
+
+}
