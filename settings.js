fdbf12eb2698d6f315e5b84d8ea6875750f25410
LITE HACK OP <mayankxmarty1@gmail.com>
2026-07-15 23:27:53 +0530

    settings.js

---
+++ settings.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAQ5CO4sLv_8j22N0YJyQmz8hIZpMs6DhE",
+  authDomain: "lhop01-admin.firebaseapp.com",
+  databaseURL: "https://lhop01-admin-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "lhop01-admin",
+  storageBucket: "lhop01-admin.firebasestorage.app",
+  messagingSenderId: "382345315199",
+  appId: "1:382345315199:web:fafa4d6ee8d82e1d97104a"
+};  
