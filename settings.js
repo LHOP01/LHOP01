c33a08d68d296eb7de73f9a8f54a46e39f05e561
LITE HACK OP <mayankxmarty1@gmail.com>
2026-07-15 23:35:48 +0530

    settings.js

--- settings.js
+++ settings.js
@@ -1,3 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+  getDatabase,
+  ref,
+  set,
+  onValue
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAQ5CO4sLv_8j22N0YJyQmz8hIZpMs6DhE",
   authDomain: "lhop01-admin.firebaseapp.com",
@@ -6,4 +15,35 @@ const firebaseConfig = {
   storageBucket: "lhop01-admin.firebasestorage.app",
   messagingSenderId: "382345315199",
   appId: "1:382345315199:web:fafa4d6ee8d82e1d97104a"
-};  
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// Save Settings
+window.saveSettings = function () {
+
+  const panelName = document.getElementById("panelName").value;
+  const announcement = document.getElementById("announcement").value;
+
+  set(ref(db, "settings"), {
+    panelName: panelName,
+    announcement: announcement
+  });
+
+  alert("Settings Saved Successfully");
+};
+
+// Load Settings
+onValue(ref(db, "settings"), (snapshot) => {
+
+  const data = snapshot.val();
+
+  if (data) {
+
+    document.getElementById("panelName").value = data.panelName || "";
+    document.getElementById("announcement").value = data.announcement || "";
+
+  }
+
+});
