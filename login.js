3cddb785c369d3a533185d86470832490dbf19bb
LITE HACK OP <mayankxmarty1@gmail.com>
2026-07-16 09:36:04 +0530

    login.js

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ function login(){
 const username = document.getElementById("username").value;
 const password = document.getElementById("password").value;
 
-if(username === "admin" && password === "123456"){
+if(username === "BRUTAL" && password === "245678"){
 
 localStorage.setItem("adminLogin","true");
 
