5a138f7e5432e44166278d8397cb50b44466952a
LITE HACK OP <mayankxmarty1@gmail.com>
2026-07-19 19:03:48 +0530

    app.js

--- app.js
+++ app.js
@@ -45,6 +45,12 @@ function generateCustomKey() {
 
 window.onload = function(){
 
+    const savedTheme = localStorage.getItem("theme");
+
+    if(savedTheme === "light"){
+        document.body.classList.add("light-theme");
+    }
+
     loadPanelSettings();
 
     loadKeys();
@@ -78,5 +84,17 @@ window.showToast = function(message){
     setTimeout(()=>{
         toast.style.display = "none";
     },2900);
+
+}
+
+window.toggleTheme = function(){
+
+    document.body.classList.toggle("light-theme");
+
+    if(document.body.classList.contains("light-theme")){
+        localStorage.setItem("theme","light");
+    }else{
+        localStorage.setItem("theme","dark");
+    }
 
 }
